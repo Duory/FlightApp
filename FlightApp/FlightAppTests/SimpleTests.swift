0ae41093685bf94aa0c829653b7ed18468078bf6
Ilyas Siraev <axillar16@gmail.com>
2019-05-31 21:35:56 +0300

    Add folders and plane icon.

---
+++ FlightApp/FlightAppTests/SimpleTests.swift
@@ -0,0 +1,16 @@
+//
+//  SimpleTests.swift
+//  FlightApp
+//
+//  Created by Ilyas Siraev on 31/05/2019.
+//  Copyright © 2019 com.onthemoon2. All rights reserved.
+//
+
+import XCTest
+@testable import FlightApp
+
+class SimpleTests: XCTestCase {
+    func testSimple() {
+        XCTAssert(true)
+    }
+}
